e407276c9ff43abc27384c63fa4c3845fe1a59db
Azat <azatuk2005@mail.ru>
2020-04-19 21:29:49 +0600

    feat: add adaptation for ipad pro

--- src/scripts/burger-menu.js
+++ src/scripts/burger-menu.js
@@ -1,6 +1,15 @@
 document.getElementById("trigger").onclick = function() {open()};
 
 function open() {
-    document.getElementById("header__container").classList.toggle("show");
-    document.getElementById("trigger").classList.toggle("trigger-move");
+    // document.getElementById('trigger__icon-menu').classList.remove('trigger__icon_active')
+    // document.getElementById('trigger__icon-cross').classList.add("trigger__icon_active")
+    document.getElementById("header__container").classList.toggle("show")
+    document.getElementById("trigger").classList.toggle("trigger-move")
+    if(document.getElementById("trigger__icon-menu").classList.contains("trigger__icon_active")) {
+        document.getElementById("trigger__icon-menu").classList.remove("trigger__icon_active")
+        document.getElementById("trigger__icon-cross").classList.add("trigger__icon_active")
+    } else {
+        document.getElementById("trigger__icon-menu").classList.add("trigger__icon_active")
+        document.getElementById("trigger__icon-cross").classList.remove("trigger__icon_active")     
+    }
 }
